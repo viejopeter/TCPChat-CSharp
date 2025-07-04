13a56a496741b5b42b66adf1abddf264c7230767
viejopeter <pquinchanegua@gmail.com>
2025-07-04 17:06:15 +1000

    Initial commit

---
+++ Windows Forms core chat/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace Windows_Forms_Chat
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.MyPortTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.serverPortTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ServerIPTextBox = new System.Windows.Forms.TextBox();
+            this.ChatTextBox = new System.Windows.Forms.TextBox();
+            this.TypeTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.HostButton = new System.Windows.Forms.Button();
+            this.JoinButton = new System.Windows.Forms.Button();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "My Port";
+            // 
+            // MyPortTextBox
+            // 
+            this.MyPortTextBox.Location = new System.Drawing.Point(13, 37);
+            this.MyPortTextBox.Name = "MyPortTextBox";
+            this.MyPortTextBox.Size = new System.Drawing.Size(125, 27);
+            this.MyPortTextBox.TabIndex = 1;
+            this.MyPortTextBox.Text = "6666";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(251, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Server Port";
+            // 
+            // serverPortTextBox
+            // 
+            this.serverPortTextBox.Location = new System.Drawing.Point(251, 37);
+            this.serverPortTextBox.Name = "serverPortTextBox";
+            this.serverPortTextBox.Size = new System.Drawing.Size(125, 27);
+            this.serverPortTextBox.TabIndex = 3;
+            this.serverPortTextBox.Text = "6666";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(423, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "server IP";
+            // 
+            // ServerIPTextBox
+            // 
+            this.ServerIPTextBox.Location = new System.Drawing.Point(423, 37);
+            this.ServerIPTextBox.Name = "ServerIPTextBox";
+            this.ServerIPTextBox.Size = new System.Drawing.Size(159, 27);
+            this.ServerIPTextBox.TabIndex = 5;
+            this.ServerIPTextBox.Text = "127.0.0.1";
+            // 
+            // ChatTextBox
+            // 
+            this.ChatTextBox.Location = new System.Drawing.Point(12, 167);
+            this.ChatTextBox.Multiline = true;
+            this.ChatTextBox.Name = "ChatTextBox";
+            this.ChatTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ChatTextBox.Size = new System.Drawing.Size(605, 205);
+            this.ChatTextBox.TabIndex = 6;
+            this.ChatTextBox.Text = "\r\n";
+            // 
+            // TypeTextBox
+            // 
+            this.TypeTextBox.Location = new System.Drawing.Point(60, 390);
+            this.TypeTextBox.Name = "TypeTextBox";
+            this.TypeTextBox.Size = new System.Drawing.Size(448, 27);
+            this.TypeTextBox.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 390);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Chat:";
+            // 
+            // HostButton
+            // 
+            this.HostButton.Location = new System.Drawing.Point(13, 91);
+            this.HostButton.Name = "HostButton";
+            this.HostButton.Size = new System.Drawing.Size(94, 29);
+            this.HostButton.TabIndex = 9;
+            this.HostButton.Text = "Host Server";
+            this.HostButton.UseVisualStyleBackColor = true;
+            this.HostButton.Click += new System.EventHandler(this.HostButton_Click);
+            // 
+            // JoinButton
+            // 
+            this.JoinButton.Location = new System.Drawing.Point(251, 91);
+            this.JoinButton.Name = "JoinButton";
+            this.JoinButton.Size = new System.Drawing.Size(94, 29);
+            this.JoinButton.TabIndex = 10;
+            this.JoinButton.Text = "Join Server";
+            this.JoinButton.UseVisualStyleBackColor = true;
+            this.JoinButton.Click += new System.EventHandler(this.JoinButton_Click);
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(523, 390);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(94, 29);
+            this.SendButton.TabIndex = 11;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(173, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "OR";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Violet;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(731, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 98);
+            this.button1.TabIndex = 13;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Violet;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(831, 34);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 98);
+            this.button2.TabIndex = 13;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Violet;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(931, 34);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 98);
+            this.button3.TabIndex = 13;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Violet;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(731, 138);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 98);
+            this.button4.TabIndex = 13;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Violet;
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(831, 138);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(94, 98);
+            this.button5.TabIndex = 13;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Violet;
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button6.Location = new System.Drawing.Point(931, 138);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(94, 98);
+            this.button6.TabIndex = 13;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Violet;
+            this.button7.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button7.Location = new System.Drawing.Point(731, 242);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(94, 98);
+            this.button7.TabIndex = 13;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Violet;
+            this.button8.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button8.Location = new System.Drawing.Point(831, 242);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(94, 98);
+            this.button8.TabIndex = 13;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Violet;
+            this.button9.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button9.Location = new System.Drawing.Point(931, 242);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(94, 98);
+            this.button9.TabIndex = 13;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1153, 478);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.JoinButton);
+            this.Controls.Add(this.HostButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TypeTextBox);
+            this.Controls.Add(this.ChatTextBox);
+            this.Controls.Add(this.ServerIPTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.serverPortTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.MyPortTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox MyPortTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox serverPortTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ServerIPTextBox;
+        private System.Windows.Forms.TextBox ChatTextBox;
+        private System.Windows.Forms.TextBox TypeTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button HostButton;
+        private System.Windows.Forms.Button JoinButton;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ Windows Forms core chat/TCPChatServer.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+
+//https://github.com/AbleOpus/NetworkingSamples/blob/master/MultiServer/Program.cs
+namespace Windows_Forms_Chat
+{
+    public class TCPChatServer : TCPChatBase
+    {
+
+        public Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        //connected clients
+        public List<ClientSocket> clientSockets = new List<ClientSocket>();
+
+        public static TCPChatServer createInstance(int port, TextBox chatTextBox)
+        {
+            TCPChatServer tcp = null;
+            //setup if port within range and valid chat box given
+            if (port > 0 && port < 65535 && chatTextBox != null)
+            {
+                tcp = new TCPChatServer();
+                tcp.port = port;
+                tcp.chatTextBox = chatTextBox;
+
+            }
+
+            //return empty if user not enter useful details
+            return tcp;
+        }
+
+        public void SetupServer()
+        {
+            chatTextBox.Text += "Setting up server...\n";
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, port));
+            serverSocket.Listen(0);
+            //kick off thread to read connecting clients, when one connects, it'll call out AcceptCallback function
+            serverSocket.BeginAccept(AcceptCallback, this);
+            chatTextBox.Text += "Server setup complete\n";
+        }
+
+
+
+        public void CloseAllSockets()
+        {
+            foreach (ClientSocket clientSocket in clientSockets)
+            {
+                clientSocket.socket.Shutdown(SocketShutdown.Both);
+                clientSocket.socket.Close();
+            }
+            clientSockets.Clear();
+            serverSocket.Close();
+        }
+
+        public void AcceptCallback(IAsyncResult AR)
+        {
+            Socket joiningSocket;
+
+            try
+            {
+                joiningSocket = serverSocket.EndAccept(AR);
+            }
+            catch (ObjectDisposedException) // I cannot seem to avoid this (on exit when properly closing sockets)
+            {
+                return;
+            }
+
+            ClientSocket newClientSocket = new ClientSocket();
+            newClientSocket.socket = joiningSocket;
+
+            clientSockets.Add(newClientSocket);
+            //start a thread to listen out for this new joining socket. Therefore there is a thread open for each client
+            joiningSocket.BeginReceive(newClientSocket.buffer, 0, ClientSocket.BUFFER_SIZE, SocketFlags.None, ReceiveCallback, newClientSocket);
+            AddToChat("Client connected, waiting for request...");
+
+            //we finished this accept thread, better kick off another so more people can join
+            serverSocket.BeginAccept(AcceptCallback, null);
+        }
+
+        public void ReceiveCallback(IAsyncResult AR)
+        {
+            ClientSocket currentClientSocket = (ClientSocket)AR.AsyncState;
+
+            int received;
+
+            try
+            {
+                received = currentClientSocket.socket.EndReceive(AR);
+            }
+            catch (SocketException)
+            {
+                AddToChat("Client forcefully disconnected");
+                // Don't shutdown because the socket may be disposed and its disconnected anyway.
+                currentClientSocket.socket.Close();
+                clientSockets.Remove(currentClientSocket);
+                return;
+            }
+
+            byte[] recBuf = new byte[received];
+            Array.Copy(currentClientSocket.buffer, recBuf, received);
+            string text = Encoding.ASCII.GetString(recBuf);
+
+            AddToChat(text);
+
+            if (text.ToLower() == "!commands") // Client requested time
+            {
+                byte[] data = Encoding.ASCII.GetBytes("Commands are !commands !about !who !whisper !exit");
+                currentClientSocket.socket.Send(data);
+                AddToChat("Commands sent to client");
+            }
+            else if (text.Contains("!username "))
+            {
+                string requestedUsername = text.Substring(10).Trim();
+                bool existUsername = clientSockets.Exists(allClients => allClients.username != null || allClients.Equals(requestedUsername));
+                if (existUsername)
+                {
+                    byte[] rejection = Encoding.ASCII.GetBytes("Username already taken");
+                    currentClientSocket.socket.Send(rejection);
+                    // Always Shutdown before closing
+                    currentClientSocket.socket.Shutdown(SocketShutdown.Both);
+                    currentClientSocket.socket.Close();
+                    clientSockets.Remove(currentClientSocket);
+                    AddToChat("Client disconnected due to duplicate username");
+                    return;
+                }
+                else
+                {
+                    currentClientSocket.username = requestedUsername;
+                    byte[] data = Encoding.ASCII.GetBytes("Username accepted");
+                    currentClientSocket.socket.Send(data);
+                    AddToChat($"User '{requestedUsername}' joined the chat.");
+                }
+            }
+            else if (text.ToLower() == "!exit") // Client wants to exit gracefully
+            {
+                // Always Shutdown before closing
+                currentClientSocket.socket.Shutdown(SocketShutdown.Both);
+                currentClientSocket.socket.Close();
+                clientSockets.Remove(currentClientSocket);
+                AddToChat("Client disconnected");
+                return;
+            }
+            else
+            {
+                //normal message broadcast out to all clients
+                SendToAll(text, currentClientSocket);
+            }
+            //we just received a message from this socket, better keep an ear out with another thread for the next one
+            currentClientSocket.socket.BeginReceive(currentClientSocket.buffer, 0, ClientSocket.BUFFER_SIZE, SocketFlags.None, ReceiveCallback, currentClientSocket);
+        }
+
+        public void SendToAll(string str, ClientSocket from)
+        {
+            foreach (ClientSocket c in clientSockets)
+            {
+                if (from == null || !from.socket.Equals(c))
+                {
+                    byte[] data = Encoding.ASCII.GetBytes(str);
+                    c.socket.Send(data);
+                }
+            }
+        }
+
+
+    }
+}
